3d3f814c540b78f825f0e3dd9b2bc5371ffdba7e
Le Vivilet <levivilet@pm.me>
2026-02-19 23:11:38 +0100

    feat: refactor getPanelDom to utilize getPanelHeaderDom and remove unused code

--- packages/panel-worker/src/parts/GetPanelDom/GetPanelDom.ts
+++ packages/panel-worker/src/parts/GetPanelDom/GetPanelDom.ts
@@ -2,32 +2,17 @@ import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { PanelState } from '../PanelState/PanelState.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import { getActionsDom } from '../GetActionsDom/GetActionsDom.ts'
-import { getGlobalActionsDom } from '../GetGlobalActionsDom/GetGlobalActionsDom.ts'
-import { getPanelTabsVirtualDom } from '../GetPanelTabsVirtualDom/GetPanelTabsVirtualDom.ts'
+import { getPanelHeaderDom } from '../GetPanelHeaderDom/GetPanelHeaderDom.ts'
 
 export const getPanelDom = (newState: PanelState): readonly VirtualDomNode[] => {
   const { childUid } = newState
-  const tabsDom = getPanelTabsVirtualDom(newState.views, newState.selectedIndex, newState.badgeCounts)
   return [
     {
       childCount: 2,
       className: ClassNames.Panel,
       type: VirtualDomElements.Div,
     },
-    {
-      childCount: 3,
-      className: ClassNames.PanelHeader,
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: newState.views.length,
-      className: ClassNames.PanelTabs,
-      type: VirtualDomElements.Div,
-    },
-    ...tabsDom,
-    ...getActionsDom(newState),
-    ...getGlobalActionsDom(),
+    ...getPanelHeaderDom(newState),
     {
       type: VirtualDomElements.Reference,
       uid: childUid,
