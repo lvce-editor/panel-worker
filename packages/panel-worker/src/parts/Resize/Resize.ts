8d1dce77f998f2d247af8fc4acc05d139a79d98a
Le Vivilet <levivilet@pm.me>
2026-02-19 17:53:46 +0100

    feat: refactor resize function to be asynchronous and handle viewlet resizing

--- packages/panel-worker/src/parts/Resize/Resize.ts
+++ packages/panel-worker/src/parts/Resize/Resize.ts
@@ -1,8 +1,23 @@
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { PanelState } from '../PanelState/PanelState.ts'
 
-export const resize = (state: PanelState, dimensions: any): PanelState => {
-  return {
+export const resize = async (state: PanelState, dimensions: any): Promise<PanelState> => {
+  // TODO
+  const currentViewletInstance = {}
+  const newState = {
     ...state,
     ...dimensions,
+  }
+  if (!currentViewletInstance) {
+    return {
+      ...newState,
+    }
+  }
+  // @ts-ignore
+  const currentViewletUid = currentViewletInstance.state.uid
+  // @ts-ignore
+  const commands = await RendererWorker.invoke('Viewlet.resize', currentViewletUid, dimensions)
+  return {
+    ...newState,
   }
 }
