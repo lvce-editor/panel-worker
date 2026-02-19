50d1f4469c884a1f97171447d37f745f99aa1fc7
Le Vivilet <levivilet@pm.me>
2026-02-19 23:30:14 +0100

    refactor: simplify selectIndex function by destructuring views from state

--- packages/panel-worker/src/parts/LoadContent/SelectIndex/SelectIndex.ts
+++ packages/panel-worker/src/parts/LoadContent/SelectIndex/SelectIndex.ts
@@ -2,7 +2,8 @@ import type { PanelState } from '../../PanelState/PanelState.ts'
 import * as OpenViewlet from '../OpenViewlet/OpenViewlet.ts'
 
 export const selectIndex = async (state: PanelState, index: number): Promise<PanelState> => {
-  await OpenViewlet.openViewlet(state, state.views[index])
+  const { views } = state
+  await OpenViewlet.openViewlet(state, views[index])
   return {
     ...state,
     selectedIndex: index,
