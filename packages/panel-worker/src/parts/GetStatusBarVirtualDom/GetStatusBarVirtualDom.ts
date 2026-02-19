8f2fef6f73e59232cbea9bed62c7cb63585b3c2b
Le Vivilet <levivilet@pm.me>
2026-02-19 16:52:23 +0100

    unused

--- packages/panel-worker/src/parts/GetStatusBarVirtualDom/GetStatusBarVirtualDom.ts
+++ packages/panel-worker/src/parts/GetStatusBarVirtualDom/GetStatusBarVirtualDom.ts
@@ -1,19 +1,5 @@
-import { type VirtualDomNode, AriaRoles, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { type VirtualDomNode, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
-import * as GetStatusBarItemsLeftDom from '../GetStatusBarItemsLeftDom/GetStatusBarItemsLeftDom.ts'
-import * as GetStatusBarItemsRightDom from '../GetStatusBarItemsRightDom/GetStatusBarItemsRightDom.ts'
-
-const getChildCount = (leftCount: number, rightCount: number): number => {
-  let count = 0
-  if (leftCount > 0) {
-    count++
-  }
-  if (rightCount > 0) {
-    count++
-  }
-  return count
-}
 
 export const getStatusBarVirtualDom = (
   statusBarItemsLeft: readonly StatusBarItem[],
@@ -21,14 +7,10 @@ export const getStatusBarVirtualDom = (
 ): readonly VirtualDomNode[] => {
   const dom: VirtualDomNode[] = [
     {
-      childCount: getChildCount(statusBarItemsLeft.length, statusBarItemsRight.length),
-      className: 'StatusBar',
-      onClick: DomEventListenerFunctions.HandleClick,
-      role: AriaRoles.Status,
       type: VirtualDomElements.Div,
+      childCount: 0,
+      className: 'Panel',
     },
-    ...GetStatusBarItemsLeftDom.getStatusBarItemsLeftDom(statusBarItemsLeft),
-    ...GetStatusBarItemsRightDom.getStatusBarItemsRightDom(statusBarItemsRight),
   ]
   return dom
 }
