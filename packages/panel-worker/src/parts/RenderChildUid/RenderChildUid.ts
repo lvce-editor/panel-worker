a3e8b56e50012b172a21cec14bec35f8a2d47660
Le Vivilet <levivilet@pm.me>
2026-02-20 00:37:01 +0100

    fix

--- packages/panel-worker/src/parts/RenderChildUid/RenderChildUid.ts
+++ packages/panel-worker/src/parts/RenderChildUid/RenderChildUid.ts
@@ -1,5 +1,6 @@
 import type { PanelState } from '../PanelState/PanelState.ts'
 
+// TODO also need to dispose child when panel becomes hidden
 export const renderChildUid = (oldState: PanelState, newState: PanelState): readonly any[] => {
   const oldChildUid = oldState.childUid
   if (oldChildUid <= 0 || oldChildUid === newState.childUid) {
