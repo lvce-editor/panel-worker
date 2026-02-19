8b132fad08e6b1a48cee12742c6472059c870240
Le Vivilet <levivilet@pm.me>
2026-02-20 00:36:40 +0100

    log

--- packages/panel-worker/src/parts/RenderChildUid/RenderChildUid.ts
+++ packages/panel-worker/src/parts/RenderChildUid/RenderChildUid.ts
@@ -2,7 +2,6 @@ import type { PanelState } from '../PanelState/PanelState.ts'
 
 export const renderChildUid = (oldState: PanelState, newState: PanelState): readonly any[] => {
   const oldChildUid = oldState.childUid
-  console.log({ newChild: newState.childUid, oldChildUid })
   if (oldChildUid <= 0 || oldChildUid === newState.childUid) {
     return []
   }
