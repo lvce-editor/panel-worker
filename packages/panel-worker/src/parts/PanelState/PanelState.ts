bf00d031d12225ea67aeff20bd198c2d93e8064c
Le Vivilet <levivilet@pm.me>
2026-02-19 16:53:11 +0100

    rename

--- packages/panel-worker/src/parts/PanelState/PanelState.ts
+++ packages/panel-worker/src/parts/PanelState/PanelState.ts
@@ -1,13 +1,9 @@
-import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
-
 export interface PanelState {
   readonly assetDir: string
   readonly disposed?: boolean
   readonly errorCount: number
   readonly initial: boolean
   readonly platform: number
-  readonly statusBarItemsLeft: readonly StatusBarItem[]
-  readonly statusBarItemsRight: readonly StatusBarItem[]
   readonly uid: number
   readonly warningCount: number
 }
