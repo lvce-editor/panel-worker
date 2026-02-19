b5553cd311c0465f7d3e11797feef1cbbc648bed
Le Vivilet <levivilet@pm.me>
2026-02-19 16:53:30 +0100

    fix

--- packages/panel-worker/src/parts/GetStatusBarVirtualDom/GetStatusBarVirtualDom.ts
+++ packages/panel-worker/src/parts/GetStatusBarVirtualDom/GetStatusBarVirtualDom.ts
@@ -1,15 +1,11 @@
 import { type VirtualDomNode, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
-import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
 
-export const getStatusBarVirtualDom = (
-  statusBarItemsLeft: readonly StatusBarItem[],
-  statusBarItemsRight: readonly StatusBarItem[],
-): readonly VirtualDomNode[] => {
+export const getStatusBarVirtualDom = (): readonly VirtualDomNode[] => {
   const dom: VirtualDomNode[] = [
     {
-      type: VirtualDomElements.Div,
       childCount: 0,
       className: 'Panel',
+      type: VirtualDomElements.Div,
     },
   ]
   return dom
