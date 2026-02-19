3d85ac1108634f94cf2aa6e31fadf9c4c70ca8d5
Le Vivilet <levivilet@pm.me>
2026-02-19 16:56:28 +0100

    unused

--- packages/panel-worker/src/parts/SaveState/SaveState.ts
+++ packages/panel-worker/src/parts/SaveState/SaveState.ts
@@ -1,10 +1,8 @@
-import type { SavedState } from '../SavedState/SavedState.ts'
 import type { PanelState } from '../PanelState/PanelState.ts'
+import type { SavedState } from '../SavedState/SavedState.ts'
 
 export const saveState = (state: PanelState): SavedState => {
-  const { statusBarItemsLeft, statusBarItemsRight } = state
   return {
-    itemsLeft: statusBarItemsLeft,
-    itemsRight: statusBarItemsRight,
+    x: 0,
   }
 }
