b7444bb35a647a5c2819e121b4246b41708ff765
Le Vivilet <levivilet@pm.me>
2026-02-19 23:14:08 +0100

    feat: refactor getPanelDom to utilize getPanelHeaderDom and remove unused code (#39)

---
+++ packages/panel-worker/src/parts/GetPanelHeaderDom/GetPanelHeaderDom.ts
@@ -0,0 +1,26 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { PanelState } from '../PanelState/PanelState.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import { getActionsDom } from '../GetActionsDom/GetActionsDom.ts'
+import { getGlobalActionsDom } from '../GetGlobalActionsDom/GetGlobalActionsDom.ts'
+import { getPanelTabsVirtualDom } from '../GetPanelTabsVirtualDom/GetPanelTabsVirtualDom.ts'
+
+export const getPanelHeaderDom = (newState: PanelState): readonly VirtualDomNode[] => {
+  const tabsDom = getPanelTabsVirtualDom(newState.views, newState.selectedIndex, newState.badgeCounts)
+  return [
+    {
+      childCount: 3,
+      className: ClassNames.PanelHeader,
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: newState.views.length,
+      className: ClassNames.PanelTabs,
+      type: VirtualDomElements.Div,
+    },
+    ...tabsDom,
+    ...getActionsDom(newState),
+    ...getGlobalActionsDom(),
+  ]
+}
