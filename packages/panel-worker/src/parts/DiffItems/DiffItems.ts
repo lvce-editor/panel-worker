0b3683a6affcf853af2fee946aac39d678c68471
Le Vivilet <levivilet@pm.me>
2026-02-19 16:55:38 +0100

    unused

--- packages/panel-worker/src/parts/DiffItems/DiffItems.ts
+++ packages/panel-worker/src/parts/DiffItems/DiffItems.ts
@@ -1,5 +1,5 @@
 import type { PanelState } from '../PanelState/PanelState.ts'
 
 export const isEqual = (oldState: PanelState, newState: PanelState): boolean => {
-  return oldState.statusBarItemsLeft === newState.statusBarItemsLeft && oldState.statusBarItemsRight === newState.statusBarItemsRight
+  return oldState.assetDir === newState.assetDir
 }
