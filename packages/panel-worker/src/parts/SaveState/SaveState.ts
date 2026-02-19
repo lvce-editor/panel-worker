d609c6eab2d3cfd47611b6d5102d26f87940c188
Le Vivilet <levivilet@pm.me>
2026-02-19 23:27:09 +0100

    feat: update saveState function to return SavedState type and modify SavedState interface (#48)

--- packages/panel-worker/src/parts/SaveState/SaveState.ts
+++ packages/panel-worker/src/parts/SaveState/SaveState.ts
@@ -1,6 +1,7 @@
 import type { PanelState } from '../PanelState/PanelState.ts'
+import type { SavedState } from '../SavedState/SavedState.ts'
 
-export const saveState = (state: PanelState): any => {
+export const saveState = (state: PanelState): SavedState => {
   const { currentViewletId } = state
   return {
     currentViewletId,
