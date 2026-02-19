932a3eb5c19bfc0de01d4de03f30d706392858e7
Le Vivilet <levivilet@pm.me>
2026-02-19 23:15:57 +0100

    feat: refactor createPanelTab to extract getTabClassName function

--- packages/panel-worker/src/parts/CreatePanelTab/CreatePanelTab.ts
+++ packages/panel-worker/src/parts/CreatePanelTab/CreatePanelTab.ts
@@ -3,12 +3,17 @@ import { AriaRoles, text, VirtualDomElements } from '@lvce-editor/virtual-dom-wo
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
-export const createPanelTab = (tab: string, badgeCount: number, isSelected: boolean, index: number): readonly VirtualDomNode[] => {
-  const label = tab
+const getTabClassName = (isSelected: boolean): string => {
   let className = ClassNames.PanelTab
   if (isSelected) {
     className += ' ' + ClassNames.PanelTabSelected
   }
+  return className
+}
+
+export const createPanelTab = (tab: string, badgeCount: number, isSelected: boolean, index: number): readonly VirtualDomNode[] => {
+  const label = tab
+  const className = getTabClassName(isSelected)
   const childCount = badgeCount ? 2 : 1
   const tabDom: VirtualDomNode = {
     ariaSelected: isSelected,
