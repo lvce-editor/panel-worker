7324a9ad70ae4236e3a1597953fb9476bcb6d546
Le Vivilet <levivilet@pm.me>
2026-02-20 11:29:44 +0100

    feat: update panel.switch-tabs test to use Command for tab selection (#71)

--- packages/e2e/src/panel-switch-tabs.ts
+++ packages/e2e/src/panel-switch-tabs.ts
@@ -4,16 +4,15 @@ export const name = 'panel.switch-tabs'
 
 export const skip = 1
 
-export const test: Test = async ({ expect, Locator, Panel }) => {
+export const test: Test = async ({ Command, expect, Locator, Panel }) => {
   // Arrange
   await Panel.openProblems()
-
   const problemsTab = Locator('.PanelTab[name="Problems"]')
   const outputTab = Locator('.PanelTab[name="Output"]')
   await expect(problemsTab).toHaveAttribute('aria-selected', 'true')
 
   // Act
-  await outputTab.click()
+  await Command.execute('Panel.selectName', 'Output')
 
   // Assert
   await expect(outputTab).toHaveAttribute('aria-selected', 'true')
