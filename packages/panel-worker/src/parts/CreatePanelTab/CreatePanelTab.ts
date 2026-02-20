45d50b163e030bf2ced477630e87ae5d698190c4
Le Vivilet <levivilet@pm.me>
2026-02-20 11:07:42 +0100

    feat: remove index parameter from createPanelTab and related usages

--- packages/panel-worker/src/parts/CreatePanelTab/CreatePanelTab.ts
+++ packages/panel-worker/src/parts/CreatePanelTab/CreatePanelTab.ts
@@ -4,7 +4,7 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as GetTabClassName from '../GetTabClassName/GetTabClassName.ts'
 
-export const createPanelTab = (tab: string, badgeCount: number, isSelected: boolean, index: number): readonly VirtualDomNode[] => {
+export const createPanelTab = (tab: string, badgeCount: number, isSelected: boolean): readonly VirtualDomNode[] => {
   const label = tab
   const className = GetTabClassName.getTabClassName(isSelected)
   const childCount = badgeCount ? 2 : 1
@@ -12,7 +12,6 @@ export const createPanelTab = (tab: string, badgeCount: number, isSelected: bool
     ariaSelected: isSelected,
     childCount,
     className,
-    'data-index': index,
     name: tab,
     onClick: DomEventListenerFunctions.HandleClickTab,
     role: AriaRoles.Tab,
