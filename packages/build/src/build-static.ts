9038cfce26bc68c17898ad962b52b52afcaff910
Le Vivilet <levivilet@pm.me>
2026-02-20 11:11:19 +0100

    feat: enhance postinstall and build-static scripts with improved file handling and URL generation (#65)

--- packages/build/src/build-static.ts
+++ packages/build/src/build-static.ts
@@ -1,4 +1,4 @@
-import { cp } from 'node:fs/promises'
+import { cp, readFile, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { pathToFileURL } from 'node:url'
 import { root } from './root.js'
@@ -10,31 +10,30 @@ const sharedProcessUrl = pathToFileURL(sharedProcessPath).toString()
 const sharedProcess = await import(sharedProcessUrl)
 
 process.env.PATH_PREFIX = '/panel-worker'
-// @ts-ignore
 const { commitHash } = await sharedProcess.exportStatic({
   root,
   extensionPath: '',
   testPath: 'packages/e2e',
 })
 
-// const rendererWorkerPath = join(root, 'dist', commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
+const rendererWorkerPath = join(root, 'dist', commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
 
 export const getRemoteUrl = (path: string): string => {
   const url = pathToFileURL(path).toString().slice(8)
   return `/remote/${url}`
 }
 
-// const content = await readFile(rendererWorkerPath, 'utf8')
-// const workerPath = join(root, '.tmp/dist/dist/panelWorkerMain.js')
-// const remoteUrl = getRemoteUrl(workerPath)
+const content = await readFile(rendererWorkerPath, 'utf8')
+const workerPath = join(root, '.tmp/dist/dist/panelWorkerMain.js')
+const remoteUrl = getRemoteUrl(workerPath)
 
-// const occurrence = `// const statusBarWorkerUrl = \`\${assetDir}/packages/panel-worker/dist/panelWorkerMain.js\`
-// const statusBarWorkerUrl = \`${remoteUrl}\``
-// const replacement = `const statusBarWorkerUrl = \`\${assetDir}/packages/panel-worker/dist/panelWorkerMain.js\``
-// if (!content.includes(occurrence)) {
-//   throw new Error('occurrence not found')
-// }
-// const newContent = content.replace(occurrence, replacement)
-// await writeFile(rendererWorkerPath, newContent)
+const occurrence = `// const panelWorkerUrl = \`\${assetDir}/packages/panel-worker/dist/panelWorkerMain.js\`
+const panelWorkerUrl = \`${remoteUrl}\``
+const replacement = `const panelWorkerUrl = \`\${assetDir}/packages/panel-worker/dist/panelWorkerMain.js\``
+if (!content.includes(occurrence)) {
+  throw new Error('occurrence not found')
+}
+const newContent = content.replace(occurrence, replacement)
+await writeFile(rendererWorkerPath, newContent)
 
 await cp(join(root, 'dist'), join(root, '.tmp', 'static'), { recursive: true })
