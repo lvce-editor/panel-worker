75dd64b3f3350b6b62997f20d7531efd2e9d9110
Le Vivilet <levivilet@pm.me>
2026-05-03 19:19:30 +0000

    feature: implement GetUid utility and update openViewlet to use it

--- packages/panel-worker/src/parts/OpenViewlet/OpenViewlet.ts
+++ packages/panel-worker/src/parts/OpenViewlet/OpenViewlet.ts
@@ -1,12 +1,13 @@
 import type { PanelState } from '../PanelState/PanelState.ts'
 import { createViewlet } from '../CreateViewlet/CreateViewlet.ts'
 import * as GetContentDimensions from '../GetContentDimensions/GetContentDimensions.ts'
+import * as GetUid from '../GetUid/GetUid.ts'
 
 export const openViewlet = async (state: PanelState, id: string, focus = false): Promise<PanelState> => {
   const childDimensions = GetContentDimensions.getContentDimensions(state)
-  const childUid = Math.random()
-  const tabId = Math.random()
-  const actionsUid = Math.random()
+  const childUid = GetUid.getUid()
+  const tabId = GetUid.getUid()
+  const actionsUid = GetUid.getUid()
   const index = state.views.indexOf(id)
   await createViewlet(id, childUid, tabId, childDimensions, '')
   return {
