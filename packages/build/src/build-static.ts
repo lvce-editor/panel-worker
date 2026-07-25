baf50390ccb4455789be4afb8973a9d39132eec7
Le Vivilet <levivilet@pm.me>
2026-07-25 20:13:29 +0000

    feature: move build scripts to typescript

--- packages/build/src/build-static.ts
+++ packages/build/src/build-static.ts
@@ -1,7 +1,7 @@
 import { cp, readFile, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { pathToFileURL } from 'node:url'
-import { root } from './root.js'
+import { root } from './root.ts'
 
 const sharedProcessPath = join(root, 'node_modules', '@lvce-editor', 'shared-process', 'index.js')
 
