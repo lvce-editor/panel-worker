b3313855b325c0fb426381baf45c2c62743f5293
Le Vivilet <levivilet@pm.me>
2026-07-26 09:51:37 +0200

    fix: focus selected panel view (#112)

--- packages/panel-worker/src/parts/SelectIndex/SelectIndex.ts
+++ packages/panel-worker/src/parts/SelectIndex/SelectIndex.ts
@@ -3,5 +3,5 @@ import * as OpenViewlet from '../OpenViewlet/OpenViewlet.ts'
 
 export const selectIndex = async (state: PanelState, index: number): Promise<PanelState> => {
   const { views } = state
-  return OpenViewlet.openViewlet(state, views[index])
+  return OpenViewlet.openViewlet(state, views[index], true)
 }
---
+++ packages/panel-worker/test/SelectIndex.test.ts
@@ -0,0 +1,29 @@
+import { expect, jest, test } from '@jest/globals'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as SelectIndex from '../src/parts/SelectIndex/SelectIndex.ts'
+
+test('selectIndex focuses the selected panel view', async () => {
+  using mockRpc = RendererWorker.registerMockRpc({
+    'Layout.createPanelViewlet': async () => {},
+  })
+  const randomSpy = jest.spyOn(Math, 'random').mockReturnValueOnce(11).mockReturnValueOnce(22).mockReturnValueOnce(33)
+  const state = {
+    ...createDefaultState(),
+    height: 200,
+    views: ['Problems', 'Terminals'],
+    width: 300,
+    x: 10,
+    y: 20,
+  }
+
+  const result = await SelectIndex.selectIndex(state, 1)
+
+  expect(mockRpc.invocations).toEqual([['Layout.createPanelViewlet', 'Terminals', 11, 22, 33, { height: 165, width: 300, x: 10, y: 55 }, '', true]])
+  expect(result).toMatchObject({
+    childUid: 11,
+    currentViewletId: 'Terminals',
+    selectedIndex: 1,
+  })
+  randomSpy.mockRestore()
+})
