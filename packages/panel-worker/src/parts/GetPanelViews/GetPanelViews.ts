e23987142e066fbc8355d81b1ccb89b91494a831
Le Vivilet <levivilet@pm.me>
2026-07-28 21:19:08 +0200

    feature: add ports panel tab (#114)

--- packages/panel-worker/src/parts/GetPanelViews/GetPanelViews.ts
+++ packages/panel-worker/src/parts/GetPanelViews/GetPanelViews.ts
@@ -1,6 +1,6 @@
 import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.ts'
 
 export const getPanelViews = (): readonly string[] => {
-  const views = [ViewletModuleId.Problems, ViewletModuleId.Output, ViewletModuleId.DebugConsole, ViewletModuleId.Terminals]
+  const views = [ViewletModuleId.Problems, ViewletModuleId.Output, ViewletModuleId.DebugConsole, ViewletModuleId.Terminals, ViewletModuleId.Ports]
   return views
 }
--- packages/panel-worker/src/parts/ViewletModuleId/ViewletModuleId.ts
+++ packages/panel-worker/src/parts/ViewletModuleId/ViewletModuleId.ts
@@ -2,6 +2,8 @@ export const DebugConsole = 'Debug Console'
 
 export const Output = 'Output'
 
+export const Ports = 'Ports'
+
 export const Problems = 'Problems'
 
 export const Terminals = 'Terminals'
