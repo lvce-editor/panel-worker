8998e515b2dc4f0d60015f113dcd8dc46b77b032
Le Vivilet <levivilet@pm.me>
2026-08-15 06:33:03 -0700

    Render directly in renderer process (#123)

--- packages/panel-worker/src/parts/Render2/Render2.ts
+++ packages/panel-worker/src/parts/Render2/Render2.ts
@@ -1,9 +1,16 @@
 import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as SourceControlStates from '../PanelStates/PanelStates.ts'
+import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
 
-export const render2 = (uid: number, diffResult: readonly number[]): readonly any[] => {
+export const render2 = (uid: number, diffResult: readonly number[]): readonly any[] | Promise<readonly any[]> => {
   const { newState, oldState } = SourceControlStates.get(uid)
   SourceControlStates.set(uid, newState, newState)
   const commands = ApplyRender.applyRender(oldState, newState, diffResult)
-  return commands
+  if (!RendererProcess.isConnected()) return commands
+  return renderDirect(uid, commands)
+}
+
+const renderDirect = async (uid: number, commands: readonly any[]): Promise<readonly any[]> => {
+  const transactionId = await RendererProcess.invoke('Viewlet.queueCommands', uid, commands)
+  return [['Viewlet.commitPending', uid, transactionId]]
 }
